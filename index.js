0d0c9ae05109fa3c9f03b384f8340e65e44cd258
Siyu <nimagangfour@gmail.com>
2019-04-18 12:19:39 +1000

    Test

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Stategy;
+
 const app = express();
+
+passprot.use(new GoogleStrategy())
+
 
 app.get('/', (req, res) => {
   res.send({
