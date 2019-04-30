d8458eaa719b5da11394aba4e3d6eaeece353dd1
Siyu <nimagangfour@gmail.com>
2019-04-30 14:15:42 +1000

    30 April

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Stategy;
+require('./models/User.js');
+require('./services/passport.js');
+
+const keys = require('./config/keys.js');
+const mongoose = require('mongoose');
 
 const app = express();
 
-passprot.use(new GoogleStrategy())
 
 
-app.get('/', (req, res) => {
-  res.send({
-    hi: 'wendywendy 小鸡鸡❤️'
-  });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+require('./routes/authRoutes.js')(app);
+
+
+mongoose.connect(keys.mongoURI);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
