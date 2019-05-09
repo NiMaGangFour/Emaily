eb71b369b93bedccd5b8dfc3b13d2cec4d7ca902
Siyu <nimagangfour@gmail.com>
2019-05-09 22:26:20 +1000

    Applied Materializecss + Componennt(NavBar)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Header from './Header';
+import Header from './Header.js';
 
 // const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
