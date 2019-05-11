ae5485df4cf06d9a676a11fc4fe43088acfd81b3
Siyu <nimagangfour@gmail.com>
2019-05-11 22:04:16 +1000

    login verification with props.auth from reducer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,11 +4,11 @@ import { connect } from 'react-redux';
 
 import * as actions from '../actions';
 import Header from './Header.js';
-
+import Landing from './Landing.js';
 // const Header = () => <h2>Header</h2>
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
-const Landing = () => <h2>Landing</h2>
+// const Landing = () => <h2>Landing</h2>
 
 class App extends Component {
   componentDidMount() {
