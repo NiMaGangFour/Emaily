453c0d3aa461736eb3a605eab2c6c676c8186376
Siyu <nimagangfour@gmail.com>
2019-05-16 00:38:20 +1000

    Use className="btn" to fix StripeCheckout appearance

--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -3,15 +3,23 @@ import StripeCheckout from 'react-stripe-checkout';
 
 class Payments extends Component {
   render() {
-    debugger;
+    // debugger;
     return (
       <StripeCheckout
+        name="Emaily📧📧"
+        description="$5💰 for 5 Email Credits"
         amount={300}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_KEY}
-      />
+      >
+        <button className="btn">
+          Add Credits
+        </button>
+
+      </StripeCheckout>
     );
   }
 }
-
+// console.log('STRIPE KEY IS', process.env.REACT_APP_STRIPE_KEY);
+// console.log('Environment is', process.env.NODE_ENV);
 export default Payments;
