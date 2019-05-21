c476992d72b986a31e03e492a3c2a85efdab4b8e
Siyu <nimagangfour@gmail.com>
2019-05-21 22:31:18 +1000

    21/04

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -4,8 +4,6 @@ module.exports = function(app) {
     app.use(proxy(['/api', '/auth/google'], { target: 'http://localhost:5000' }));
 }
 
-
-
 // module.exports = function(app) {
 //   app.use(proxy('/auth/google', { target: 'http://localhost:5000/' }))
 //   app.use(proxy('/api/*', { target: 'http://localhost:5000/' }))
