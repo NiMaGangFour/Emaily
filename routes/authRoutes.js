124d0fac12c6e14929959e41cb159e93bf555c56
Siyu <nimagangfour@gmail.com>
2019-06-01 20:32:39 +1000

    change Public IP then change MongoAtlas Whitelist

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,33 +1,34 @@
-const passport = require('passport');
+const passport = require("passport");
 
 module.exports = app => {
   app.get(
-    '/auth/google',
-    passport.authenticate('google', {
-      scope: ['profile', 'email']
+    "/auth/google",
+    passport.authenticate("google", {
+      scope: ["profile", "email"]
     })
   );
 
-  app.get('/auth/google/callback',
-   passport.authenticate('google'),
-   (req, res) => {
-     res.redirect('/surveys');
-   }
- );
+  app.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+      res.redirect("/surveys");
+    }
+  );
 
-  app.get('/api/current_user', (req, res) => {
+  app.get("/api/current_user", (req, res) => {
     res.send(req.user);
   });
 
-  app.get('/api/logout', (req, res) => {
+  app.get("/api/logout", (req, res) => {
     req.logout();
-    res.redirect('/');
+    res.redirect("/");
     // res.send(req.user);
   });
 
-  app.get('/', (req, res) => {
+  app.get("/", (req, res) => {
     res.send({
-      hi: '❤️wendy❤️wendy❤️'
+      hi: "❤️wendy❤️wendy❤️❤️"
     });
   });
 
@@ -36,4 +37,4 @@ module.exports = app => {
   //     hi: '❤️❤️❤️'
   //   });
   // });
-}
+};
