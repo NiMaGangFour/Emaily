e849da1224e6895933a28d5b712fe5fea8b36dfe
Siyu <nimagangfour@gmail.com>
2019-06-06 23:26:36 +1000

    Add "REACT_APP_STRIPE_KEY" as key value pair on Heroku

--- index.js
+++ index.js
@@ -32,12 +32,11 @@ if (process.env.NODE_ENV === "production") {
 
   //Express will serve up the index.html file
   //if it doesn't recognize the route
+  app.use("*", express.static("client/build"));
   // const path = require("path");
   // app.get("*", (req, res) => {
   //   res.sendFile(path, resolve(__dirname, "client", "build", "index.html"));
   // });
-
-  app.use("*", express.static("client/build"));
 }
 
 mongoose.connect(keys.mongoURI);
