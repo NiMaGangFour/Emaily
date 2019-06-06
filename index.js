28138a9024d572f733a54e052ffc9dac94c42c76
Siyu <nimagangfour@gmail.com>
2019-06-06 22:52:08 +1000

    06

--- index.js
+++ index.js
@@ -32,10 +32,12 @@ if (process.env.NODE_ENV === "production") {
 
   //Express will serve up the index.html file
   //if it doesn't recognize the route
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path, resolve(__dirname, "client", "build", "index.html"));
-  });
+  // const path = require("path");
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path, resolve(__dirname, "client", "build", "index.html"));
+  // });
+
+  app.use("*", express.static("client/build"));
 }
 
 mongoose.connect(keys.mongoURI);
