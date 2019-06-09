087fab10104584156ba9212ed95cf8ccd1283a29
Siyu <nimagangfour@gmail.com>
2019-06-09 22:13:58 +1000

    axio.post(/api/surveys, survey)

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,11 @@ import reduxThunk from "redux-thunk";
 import App from "./components/App";
 import reducers from "./reducers";
 
+//Development for sendGrid Helper only
+import axios from "axios";
+window.axios = axios;
+//const survey = {title:'my title', subject: 'my sub', recipients:'nimagangfour@gmail.com', body:'123321123'};
+//axios.post("/api/surveys", survey);
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,22 +1,29 @@
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin.js");
 const requireCredits = require("../middlewares/requireCredits");
-
+const Mailer = require("../services/Mailer.js");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate.js");
+//create model of survey and update it
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  app.post("api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+    // is user logged in requireLogin
+    // has enough credits requireCredits
+    //pass these properties from front end
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
       title: title,
       subject: subject,
       body: body,
-      recipients: recipients.split(",").map(email => {
-        return { email: email.trim() };
-      }),
+      recipients: recipients.split(",").map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    //send email (data, template)
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+    mailer.send();
   });
 };
