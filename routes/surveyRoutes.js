ee8f26572969f48dca59f4b6d0256f2a35f824e2
Siyu <nimagangfour@gmail.com>
2019-06-29 23:33:07 +1000

    this.renderSurveys()

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,6 +10,14 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate.js");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
+  app.get("/api/surveys", requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id }).select({
+      recipients: false
+    });
+
+    res.send(surveys);
+  });
+
   app.get("/api/surveys/:surveyId/:choice", (req, res) => {
     res.send("Thans for voting!");
     // res.sendFile("../services/emailTemplates/RedirectDomainTemplate.js", {
